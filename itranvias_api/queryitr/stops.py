ead997d078964c30840f04e409d7da44a4fbad94
alfonsoperezs <fonpesan@gmail.com>
2025-04-25 23:59:06 +0200

    Add get information of stops

--- itranvias_api/queryitr/stops.py
+++ itranvias_api/queryitr/stops.py
@@ -31,3 +31,50 @@ def get_stop_buses(stop_id: int) -> dict[int, list[Bus]]:
         lines[line["linea"]] = buses
 
     return lines
+
+def get_all_stops() -> list[Stop]:
+    """
+    Get information of all stops
+
+    :return: A stop list with all the existing stops
+    """
+    response = _queryitr_adapter.get(func=7, dato="20160101T000000_gl_0_20160101T000000")
+    data = response.data
+    stops = []
+    for stop in data["iTranvias"]["actualizacion"]["paradas"]:
+        stops.append(_parse_stop(stop,data))
+    return stops
+
+def get_stop_by_id(stop_id: int) -> Stop | None:
+    """
+    Get information of about a stop
+
+    :param stop_id: The id of the stop to consult
+
+    :return: The information of the stop
+    """
+    response = _queryitr_adapter.get(func=7, dato="20160101T000000_gl_0_20160101T000000")
+    data = response.data
+    stop = next((s for s in data["iTranvias"]["actualizacion"]["paradas"] if s["id"] == stop_id), None)
+    if stop == None:
+        return None
+    return _parse_stop(stop, data)
+
+def _parse_stop(stop, data) -> Stop:
+    lines = []
+    for line_id in stop["enlaces"]:
+        line = next((l for l in data["iTranvias"]["actualizacion"]["lineas"] if l["id"] == line_id), None)
+        lines.append(Line(
+            id=line_id,
+            name=line["lin_comer"],
+            color=line["color"],
+            origin=Stop(name=line["nombre_orig"]),
+            destination=Stop(name=line["nombre_dest"]),
+        ))
+    return Stop(
+        id=stop["id"],
+        name=stop["nombre"],
+        connections=lines,
+        long=stop["posx"],
+        lat=stop["posy"],
+    )
